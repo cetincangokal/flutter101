6cd98e3b39b09845a3ec4ab3644adf43807f5c9e
cetincangokal <106526079+cetincangokal@users.noreply.github.com>
2023-01-19 18:59:54 +0300

    animated finish

--- flutter_application_1/lib/202/animated_learn.dart
+++ flutter_application_1/lib/202/animated_learn.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+const double kZero = 0;
+
 class animatedLearn extends StatefulWidget {
   const animatedLearn({super.key});
 
@@ -7,34 +9,97 @@ class animatedLearn extends StatefulWidget {
   State<animatedLearn> createState() => _animatedLearnState();
 }
 
-class _animatedLearnState extends State<animatedLearn> {
+class _animatedLearnState extends State<animatedLearn>
+    with TickerProviderStateMixin {
 //Text('data', style: context.textTheme().headline1)
   bool _isVisible = false;
+  bool _isOpacity = false;
+  late AnimationController controller;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    controller =
+        AnimationController(vsync: this, duration: _durationItems.durationLow);
+  }
+
   void changeVisible() {
     setState(() {
       _isVisible = !_isVisible;
     });
   }
 
+  void changeOpacity() {
+    setState(() {
+      _isOpacity = !_isOpacity;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: _placeHolderWidget(),),
+        appBar: AppBar(
+          title: _placeHolderWidget(),
+        ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
             changeVisible();
+            controller.animateTo(_isVisible ? 1 : kZero);
           },
         ),
-        body: _placeHolderWidget());
+        body: Column(
+          children: [
+            ListTile(
+              title: AnimatedOpacity(
+                duration: _durationItems.durationLow,
+                opacity: _isOpacity ? 1 : kZero,
+                child: Text('data'),
+              ),
+              trailing: IconButton(
+                onPressed: (() {
+                  changeOpacity();
+                }),
+                icon: Icon(Icons.precision_manufacturing_rounded),
+              ),
+            ),
+            AnimatedDefaultTextStyle(
+                child: Text('data'),
+                style: (_isVisible
+                        ? context.textTheme().headline1
+                        : context.textTheme().subtitle1) ??
+                    TextStyle(),
+                duration: _durationItems.durationLow),
+            AnimatedIcon(icon: AnimatedIcons.menu_close, progress: controller),
+            AnimatedContainer(
+              duration: _durationItems.durationLow,
+              height:
+                  _isVisible ? kZero : MediaQuery.of(context).size.width * 0.2,
+              width: MediaQuery.of(context).size.height * 0.2,
+              color: Colors.blue,
+              margin: EdgeInsets.all(5),
+            ),
+            Expanded(child: Stack(
+              children: [
+                AnimatedPositioned(top: 30,curve: Curves.elasticOut, child: Text('data', style: context.textTheme().headline1,), duration: _durationItems.durationLow)
+              ],
+            )),
+            Expanded(
+              child: AnimatedList(itemBuilder: (context, index, animation) {
+                return Text('data');
+              }),
+            ) 
+          ],
+        ));
   }
 
   AnimatedCrossFade _placeHolderWidget() {
     return AnimatedCrossFade(
-        firstChild: Placeholder(),
-        secondChild: SizedBox.shrink(),
-        duration: _durationItems.durationLow,
-        crossFadeState: _isVisible ? CrossFadeState.showFirst : CrossFadeState.showSecond,
-      );
+      firstChild: Placeholder(),
+      secondChild: SizedBox.shrink(),
+      duration: _durationItems.durationLow,
+      crossFadeState:
+          _isVisible ? CrossFadeState.showFirst : CrossFadeState.showSecond,
+    );
   }
 }
 
